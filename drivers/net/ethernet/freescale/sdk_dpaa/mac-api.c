1fe63b9b45af4a1ce1f5f3c37a53c3a106812183
Camelia Groza <camelia.groza@nxp.com>
2017-08-31 13:16:47 +0300

    dpaa_eth: memac: set adjust_link callback for fixed link interfaces
    Source: https://github.com/Freescale/linux-fslc.git
    MR: 107408
    Type: Integration
    Disposition: Merged from branch 5.4.y+qoriq+fslc
    ChangeID: 14366776de02b5e756901d5bc4a01d4dafd79b2d
    Description:
    
    Signed-off-by: Camelia Groza <camelia.groza@nxp.com>
    Signed-off-by: Purushothama Siddaiah <psiddaiah@mvista.com>
    Reviewed-by: Satya Jena <sjena@mvista.com>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- drivers/net/ethernet/freescale/sdk_dpaa/mac-api.c
+++ drivers/net/ethernet/freescale/sdk_dpaa/mac-api.c
@@ -504,15 +504,9 @@ static int memac_init_phy(struct net_device *net_dev,
 	struct phy_device       *phy_dev;
 	int err;
 
-	if (of_phy_is_fixed_link(mac_dev->phy_node)) {
-		err = of_phy_register_fixed_link(mac_dev->phy_node);
-		if (err)
-			return err;
-		phy_dev = of_phy_connect(net_dev, mac_dev->phy_node,
-				&adjust_link, 0, mac_dev->phy_if);
-
-	} else if ((macdev2enetinterface(mac_dev) == e_ENET_MODE_XGMII_10000) ||
-		   (macdev2enetinterface(mac_dev) == e_ENET_MODE_SGMII_2500)) {
+	if ((macdev2enetinterface(mac_dev) == e_ENET_MODE_XGMII_10000) ||
+	    (macdev2enetinterface(mac_dev) == e_ENET_MODE_SGMII_2500) ||
+	    of_phy_is_fixed_link(mac_dev->phy_node)) {
 		phy_dev = of_phy_connect(net_dev, mac_dev->phy_node,
 					 &adjust_link_void, 0,
 					 mac_dev->phy_if);
