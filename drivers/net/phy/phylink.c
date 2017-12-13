7c65d9dfe810953e60ba5b7769dd8e8f93aaf334
Florian Fainelli <f.fainelli@gmail.com>
2017-12-12 16:00:25 -0800

    net: phy: phylink: Allow specifying PHY device flags
    Source: Kernel.org
    MR: 96659
    Type: Integration
    Disposition: Backport from v4.16-rc1
    ChangeID: 0a62964c9020208c08c8b0323db9ea0c5980876f
    Description:
    
    In order to let subsystems like DSA fully utilize PHYLINK, we need to be able
    to communicate phy_device::flags from of_phy_{connect,attach} even when using
    PHYLINK APIs.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Niyas Ahamed Mydeen <nmydeen@mvista.com>
    Reviewed-by: Purushothama Siddaiah <psiddaiah@mvista.com>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- drivers/net/phy/phylink.c
+++ drivers/net/phy/phylink.c
@@ -717,7 +717,20 @@ int phylink_connect_phy(struct phylink *pl, struct phy_device *phy)
 }
 EXPORT_SYMBOL_GPL(phylink_connect_phy);
 
-int phylink_of_phy_connect(struct phylink *pl, struct device_node *dn)
+/**
+ * phylink_of_phy_connect() - connect the PHY specified in the DT mode.
+ * @pl: a pointer to a &struct phylink returned from phylink_create()
+ * @dn: a pointer to a &struct device_node.
+ * @flags: PHY-specific flags to communicate to the PHY device driver
+ *
+ * Connect the phy specified in the device node @dn to the phylink instance
+ * specified by @pl. Actions specified in phylink_connect_phy() will be
+ * performed.
+ *
+ * Returns 0 on success or a negative errno.
+ */
+int phylink_of_phy_connect(struct phylink *pl, struct device_node *dn,
+			   u32 flags)
 {
 	struct device_node *phy_node;
 	struct phy_device *phy_dev;
@@ -741,7 +754,8 @@ int phylink_of_phy_connect(struct phylink *pl, struct device_node *dn)
 		return 0;
 	}
 
-	phy_dev = of_phy_attach(pl->netdev, phy_node, 0, pl->link_interface);
+	phy_dev = of_phy_attach(pl->netdev, phy_node, flags,
+				pl->link_interface);
 	/* We're done with the phy_node handle */
 	of_node_put(phy_node);
 
