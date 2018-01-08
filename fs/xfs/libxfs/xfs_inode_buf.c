6155a5bf8b62fb8f0068dc555f9fa1e53f699a84
Darrick J. Wong <darrick.wong@oracle.com>
2018-01-08 10:51:04 -0800

    xfs: verify dinode header first
    Source: https://git.kernel.org
    MR: 96664
    Type: Security Fix
    Disposition: Backport from v4.16-r1
    ChangeID: 50aa90ef03007beca2c9108993f5b4f2bb4f0a66
    Description:
    
    CVE-2018-10322 kernel: Invalid pointer dereference in
    xfs_ilock_attr_map_shared() when mounting crafted xfs image
    allowing denial of service
    
    Dependency patch #5.
    
    Move the v3 inode integrity information (crc, owner, metauuid) before we
    look at anything else in the inode so that we don't waste time on a torn
    write or a totally garbled block.  This makes xfs_dinode_verify more
    consistent with the other verifiers.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- fs/xfs/libxfs/xfs_inode_buf.c
+++ fs/xfs/libxfs/xfs_inode_buf.c
@@ -392,6 +392,19 @@ xfs_dinode_verify(
 	if (dip->di_magic != cpu_to_be16(XFS_DINODE_MAGIC))
 		return false;
 
+	/* Verify v3 integrity information first */
+	if (dip->di_version >= 3) {
+		if (!xfs_sb_version_hascrc(&mp->m_sb))
+			return false;
+		if (!xfs_verify_cksum((char *)dip, mp->m_sb.sb_inodesize,
+				      XFS_DINODE_CRC_OFF))
+			return false;
+		if (be64_to_cpu(dip->di_ino) != ino)
+			return false;
+		if (!uuid_equal(&dip->di_uuid, &mp->m_sb.sb_meta_uuid))
+			return false;
+	}
+
 	/* don't allow invalid i_size */
 	if (be64_to_cpu(dip->di_size) & (1ULL << 63))
 		return false;
@@ -407,16 +420,6 @@ xfs_dinode_verify(
 	/* only version 3 or greater inodes are extensively verified here */
 	if (dip->di_version < 3)
 		return true;
-
-	if (!xfs_sb_version_hascrc(&mp->m_sb))
-		return false;
-	if (!xfs_verify_cksum((char *)dip, mp->m_sb.sb_inodesize,
-			      XFS_DINODE_CRC_OFF))
-		return false;
-	if (be64_to_cpu(dip->di_ino) != ino)
-		return false;
-	if (!uuid_equal(&dip->di_uuid, &mp->m_sb.sb_meta_uuid))
-		return false;
 
 	flags = be16_to_cpu(dip->di_flags);
 	flags2 = be64_to_cpu(dip->di_flags2);
