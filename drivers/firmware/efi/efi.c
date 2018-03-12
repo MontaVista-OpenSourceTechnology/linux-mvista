477d14c58afa25f647e82b7f451f322b1cb8e948
Sai Praneeth <sai.praneeth.prakhya@intel.com>
2018-03-12 08:44:56 +0000

    efi: Use efi_mm in x86 as well as ARM
    Source: https://git.kernel.org
    MR: 98404
    Type: Security Fix
    Disposition: Backport from v4.17-rc1
    ChangeID: 7e904a91bf6049071ef9d605a52f863ae774081d
    Description:
    
    CVE-2019-12380 kernel: memory allocation failure in the efi subsystem
    leads to denial of service
    
    Dependency Patch #1.
    
    Presently, only ARM uses mm_struct to manage EFI page tables and EFI
    runtime region mappings. As this is the preferred approach, let's make
    this data structure common across architectures. Specially, for x86,
    using this data structure improves code maintainability and readability.
    
    Tested-by: Bhupesh Sharma <bhsharma@redhat.com>
    [ardb: don't #include the world to get a declaration of struct mm_struct]
    Signed-off-by: Sai Praneeth Prakhya <sai.praneeth.prakhya@intel.com>
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Matt Fleming <matt@codeblueprint.co.uk>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Lee, Chun-Yi <jlee@suse.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ravi Shankar <ravi.v.shankar@intel.com>
    Cc: Ricardo Neri <ricardo.neri@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: linux-efi@vger.kernel.org
    Link: http://lkml.kernel.org/r/20180312084500.10764-2-ard.biesheuvel@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- drivers/firmware/efi/efi.c
+++ drivers/firmware/efi/efi.c
@@ -75,6 +75,16 @@ static unsigned long *efi_tables[] = {
 };
 
 static bool disable_runtime = IS_ENABLED(CONFIG_PREEMPT_RT_BASE);
+
+struct mm_struct efi_mm = {
+	.mm_rb			= RB_ROOT,
+	.mm_users		= ATOMIC_INIT(2),
+	.mm_count		= ATOMIC_INIT(1),
+	.mmap_sem		= __RWSEM_INITIALIZER(efi_mm.mmap_sem),
+	.page_table_lock	= __SPIN_LOCK_UNLOCKED(efi_mm.page_table_lock),
+	.mmlist			= LIST_HEAD_INIT(efi_mm.mmlist),
+};
+
 static int __init setup_noefi(char *arg)
 {
 	disable_runtime = true;
