0df87e835777b5f5b003136a0ee92c21ec293e9f
Marc Zyngier <marc.zyngier@arm.com>
2018-07-20 10:53:07 +0100

    arm64: ssbd: Introduce thread flag to control userspace mitigation
    Source: kernel.org
    MR: 94992
    Type: Integration
    Disposition: Merged from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-4.14.y
    ChangeID: e7d02797288f12ca257cdab49d19ccf9b3d2cb12
    Description:
    
    commit 9dd9614f5476687abbff8d4b12cd08ae70d7c2ad upstream.
    
    In order to allow userspace to be mitigated on demand, let's
    introduce a new thread flag that prevents the mitigation from
    being turned off when exiting to userspace, and doesn't turn
    it on on entry into the kernel (with the assumption that the
    mitigation is always enabled in the kernel itself).
    
    This will be used by a prctl interface introduced in a later
    patch.
    
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -94,7 +94,8 @@ void arch_setup_new_exec(void);
 #define TIF_RESTORE_SIGMASK	20
 #define TIF_SINGLESTEP		21
 #define TIF_32BIT		22	/* AARCH32 process */
-#define TIF_32BIT_AARCH64	23	/* 32 bit process on AArch64(ILP32) */
+#define TIF_SSBD		23	/* Wants SSB mitigation */
+#define TIF_32BIT_AARCH64	24	/* 32 bit process on AArch64(ILP32) */
 
 #define _TIF_SIGPENDING		(1 << TIF_SIGPENDING)
 #define _TIF_NEED_RESCHED	(1 << TIF_NEED_RESCHED)
