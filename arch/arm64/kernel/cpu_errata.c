8bc3037c0282834ad8a9e15aff78c58f5e91b94e
David Daney <david.daney@cavium.com>
2018-08-22 14:14:51 -0700

    arm64: Add MIDR values for Cavium cn83XX SoCs
    Source: Cavium, Inc
    MR: 95631
    Type: Integration
    Disposition: Merged from Cavium ThunderX tree
    ChangeID: d24377ab1690fcbaef042a83ee1dde74b84748b0
    Description:
    
    Tested-by: Alexander Graf <agraf@suse.de>
    Acked-by: David Daney <david.daney@cavium.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    
    Backported to 4.14
    
    Signed-off-by: Sergey Temerkhanov <stemerkhanov@cavium.com>
    Signed-off-by: Peter Swain <peter.swain@marvell.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -513,7 +513,7 @@ const struct arm64_cpu_capabilities arm64_errata[] = {
 	/* Cavium ThunderX, T83 pass 1.0 */
 		.desc = "Cavium erratum 30115",
 		.capability = ARM64_WORKAROUND_CAVIUM_30115,
-		MIDR_RANGE(MIDR_THUNDERX_83XX, 0x00, 0x00),
+		MIDR_RANGE(MIDR_OCTEON_T83, 0x00, 0x00),
 	},
 #endif
 	{
