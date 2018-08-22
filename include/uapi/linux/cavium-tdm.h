82cf46588959aaf8eac566b9cac147c2f2446c87
Peter Swain <pswain@cavium.com>
2018-08-22 14:14:53 -0700

    misc/cavium-tdm: fixed-slotmask version
    Source: Cavium, Inc
    MR: 95631
    Type: Integration
    Disposition: Merged from Cavium ThunderX tree
    ChangeID: 56bb854903781a2a4282a9c49e205f6539b9ad78
    Description:
    
    Two modes of operation:
    - dynamic read-or-write binding to any consecutive group
      of slots, causing engine teardown/restart, and potential
      data loss on other slot bindings active at the time.
    
    - static-slotmask, using ioctl(TDMSSSLOT) before any bindings
      are opened, sets a fixed mask of slots for each of read/write.
      Subsequent read-or-write bindings do not disturb other conversations
    
    Much code rework, some of which was to handle the unreleased dynamic
    slotmap proxy-engine dance, which added an internal split between
    the tdm_bus concept (on the wire) and tdm_engine, one or more of which
    are used to implement each bus.
    Most of the dynamic-slotmap infrastructure is stubbed out, but some
    excess complexity remains.
    
    ioctls added/changed:
    - TDMSSSLOT/TDMGSSLOT - set/get static slotmask
    
    - TDMSDROP - detach a previously setup bus
    
    - TDMSSILENCE - tx-underrun fill
        Allows a codec-specific "silence" pattern to be specified.
        Injected when any slot being transmitted has an empty Tx-FIFO.
        Defaults to A-Law alternating +0/-0, or continuous '@' if DEBUG.
        Specified as 32bit, for 4 consecutive sample bytes.
    
    - TDMxxxx_ ioctls preserve the old numbers of some ioctls renumbered to
        avoid clash with some compat_ioctl magic numbers, so same schema can
        be used across aarch64/mips
    
    Hardware register structs now use __BITFIELD_FIELD() compositor to
        reduce duplicate, order-swapped structs and ifdefs by importing
        an endian-helper from arch/mips
    
    preened Kconfig & Doc, require DMA_CMA
    
    fix cpu detection, preening names/whitespace
        cn81p1.0 AND p1.1 must be protected against TX on engines 1..3
        (comment was right, code was wrong)
    
    Simplified internal naming (number of dirs now DIRS, not TXRX, etc)
    
    default _MAX_SLOTS to 256
        Half of the hardware size, reducing bloat, adjust as needed
    
    Signed-off-by: Peter Swain <peter.swain@marvell.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/uapi/linux/cavium-tdm.h
+++ include/uapi/linux/cavium-tdm.h
@@ -2,83 +2,64 @@
 #define _UAPI_CAVM_TDM_H
 /* user interface to Octeon/OcteonTX TDM engines */
 
-#include <linux/types.h>
-#include <linux/ioctl.h>
+#include <asm/byteorder.h>
 
-#define TDM_VER		1	/* ioctl API version */
+#define TDM_VER		3	/* ioctl API version */
 #define NR_TDM_ENGINES	4
 #define NR_CLKS		2
-/* FIXME: dynamic: default 9, settable as TDMSSCALE(log2_slots), min 5 */
 #define TDM_SLOT_SHIFT	9	/* log2(8*64), matching tx/rxmsk[0..7] */
 #define TDM_MAX_SLOTS	(1 << TDM_SLOT_SHIFT)
+enum { tdm_tx = 0, tdm_rx = 1, TDM_DIRS = 2, };
+
+/* from mips, useful all bi-endian archs, perhaps could move to asm-generic? */
+#ifndef __BITFIELD_FIELD
+# ifdef __BIG_ENDIAN_BITFIELD
+#  define __BITFIELD_FIELD(field, more)	field; more
+# else /* little-endian */
+#  define __BITFIELD_FIELD(field, more)	more field;
+# endif /* little-endian */
+#endif /* !__BITFIELD_FIELD */
 
 /* some TDM hardware registers are visible thru ioctl()s */
 union cavium_tdm_clk_cfg {
-	__u64 u64;
+	uint64_t u64;
 	struct {
-#ifdef __BIG_ENDIAN_BITFIELD
-		__u64 fsyncgood:1;
-		__u64 reserved_48_62:15;
-		__u64 fsyncsamp:16;
-		__u64 reserved_26_31:6;
-		__u64 fsynclen:5;
-		__u64 fsyncloc:5;
-		__u64 numslots:10;
-		__u64 extrabit:1;
-		__u64 bitlen:2;
-		__u64 bclkpol:1;
-		__u64 fsyncpol:1;
-		__u64 ena:1;
-#else
-		__u64 ena:1;
-		__u64 fsyncpol:1;
-		__u64 bclkpol:1;
-		__u64 bitlen:2;
-		__u64 extrabit:1;
-		__u64 numslots:10;
-		__u64 fsyncloc:5;
-		__u64 fsynclen:5;
-		__u64 reserved_26_31:6;
-		__u64 fsyncsamp:16;
-		__u64 reserved_48_62:15;
-		__u64 fsyncgood:1;
-#endif
+		__BITFIELD_FIELD(uint64_t fsyncgood:1,
+		__BITFIELD_FIELD(uint64_t reserved_48_62:15,
+		__BITFIELD_FIELD(uint64_t fsyncsamp:16,
+		__BITFIELD_FIELD(uint64_t reserved_26_31:6,
+		__BITFIELD_FIELD(uint64_t fsynclen:5,
+		__BITFIELD_FIELD(uint64_t fsyncloc:5,
+		__BITFIELD_FIELD(uint64_t numslots:10,
+		__BITFIELD_FIELD(uint64_t extrabit:1,
+		__BITFIELD_FIELD(uint64_t bitlen:2,
+		__BITFIELD_FIELD(uint64_t bclkpol:1,
+		__BITFIELD_FIELD(uint64_t fsyncpol:1,
+		__BITFIELD_FIELD(uint64_t ena:1,
+		;))))))))))))
 	} s;
 };
 
 union cavium_tdm_clk_gen {
-	__u64 u64;
+	uint64_t u64;
 	struct {
-#ifdef __BIG_ENDIAN_BITFIELD
-		__u64 deltasamp:16;
-		__u64 numsamp:16;
-		__u64 divisor:32; /* called N in hardware ref manual */
-#else
-		__u64 divisor:32;
-		__u64 numsamp:16;
-		__u64 deltasamp:16;
-#endif
+		__BITFIELD_FIELD(uint64_t deltasamp:16,
+		__BITFIELD_FIELD(uint64_t numsamp:16,
+		__BITFIELD_FIELD(uint64_t divisor:32, /* aka N in HW docs */
+		;)))
 	} s;
 };
 
 union cavium_tdm_cfg {
-	__u64 u64;
+	uint64_t u64;
 	struct {
-#ifdef __BIG_ENDIAN_BITFIELD
-		__u64 drvtim:16;
-		__u64 samppt:16;
-		__u64 reserved_3_31:29;
-		__u64 lsbfirst:1;
-		__u64 useclk1:1;
-		__u64 enable:1;
-#else
-		__u64 enable:1;
-		__u64 useclk1:1;
-		__u64 lsbfirst:1;
-		__u64 reserved_3_31:29;
-		__u64 samppt:16;
-		__u64 drvtim:16;
-#endif
+		__BITFIELD_FIELD(uint64_t drvtim:16,
+		__BITFIELD_FIELD(uint64_t samppt:16,
+		__BITFIELD_FIELD(uint64_t reserved_3_31:29,
+		__BITFIELD_FIELD(uint64_t lsbfirst:1,
+		__BITFIELD_FIELD(uint64_t useclk1:1,
+		__BITFIELD_FIELD(uint64_t enable:1,
+		;))))))
 	} s;
 };
 
@@ -98,39 +79,47 @@ enum {
 	tdm_irqall	= 0xff,
 };
 
-/* some ioctl()s map to TDM CSR reads/writes, and are numbered accordingly */
+/* where ioctl()s map to TDM CSR reads/writes, use analogous numbers */
 #define TDM_IOCTL	'p'
-#define TDMGCLKCFG	_IOR(TDM_IOCTL, 0x0, union cavium_tdm_clk_cfg)
-#define TDMSCLKCFG	_IOW(TDM_IOCTL, 0x0, union cavium_tdm_clk_cfg)
-#define TDMGCLKGEN	_IOR(TDM_IOCTL, 0x8, union cavium_tdm_clk_gen)
-#define TDMSCLKGEN	_IOW(TDM_IOCTL, 0x8, union cavium_tdm_clk_gen)
+/* legacy values, clashing with builtin compat_ioctl values on MIPS/k4.9 ... */
+#define TDMGCLKCFG_	_IOR(TDM_IOCTL, 0x0, union cavium_tdm_clk_cfg)
+#define TDMSCLKCFG_	_IOW(TDM_IOCTL, 0x0, union cavium_tdm_clk_cfg)
+#define TDMGCLKGEN_	_IOR(TDM_IOCTL, 0x8, union cavium_tdm_clk_gen)
+#define TDMSCLKGEN_	_IOW(TDM_IOCTL, 0x8, union cavium_tdm_clk_gen)
+/* += 0x20 to avoid clash */
+#define TDMGCLKCFG	_IOR(TDM_IOCTL, 0x20, union cavium_tdm_clk_cfg)
+#define TDMSCLKCFG	_IOW(TDM_IOCTL, 0x20, union cavium_tdm_clk_cfg)
+#define TDMGCLKGEN	_IOR(TDM_IOCTL, 0x28, union cavium_tdm_clk_gen)
+#define TDMSCLKGEN	_IOW(TDM_IOCTL, 0x28, union cavium_tdm_clk_gen)
+/* but these are safe ... */
 #define TDMGTIM		_IOR(TDM_IOCTL, 0x10, union cavium_tdm_cfg)
 #define TDMSTIM		_IOW(TDM_IOCTL, 0x10, union cavium_tdm_cfg)
 
 /* other ioctl()s have numbers not divisible by 8 */
-#define TDMGFRMHZ	_IOR(TDM_IOCTL, 0x5, __u64)	/* get frame freq */
+#define TDMGFRMHZ	_IOR(TDM_IOCTL, 0x5, uint64_t)	/* get frame freq */
 #define TDMGCLKSEL	_IO(TDM_IOCTL, 0x9)	/* get clock selector */
 #define TDMSCLKSEL	_IO(TDM_IOCTL, 0xa)	/* set clock selector */
-#define TDMGSCLK	_IOR(TDM_IOCTL, 0xb, __u64)	/* get SCLK */
+#define TDMGSCLK	_IOR(TDM_IOCTL, 0xb, uint64_t)	/* get SCLK */
 
 /* get user-visible state of TDM engine */
 struct cavium_tdm_stat {
-	__u16 ver;		/* version of the tdm API */
-	__u16 supers;
-	__u8 int_sum;	/* events seen since last TDMGSTAT */
-	__u64 rxaddr;	/* DMA offset of last Rx sample */
-	__u64 txaddr;	/* DMA offset of last Tx sample */
-	__u64 rxsize;	/* size of current Rx sample space */
-	__u64 txsize;	/* size of current Tx sample space */
-	__u64 rxlimit;	/* size of config'd Rx frame space */
-	__u64 txlimit;	/* size of config'd Tx frame space */
+	uint16_t ver;		/* version of the tdm API */
+	uint16_t rx_supers;	/* DMA buffer size in superframes */
+	uint16_t tx_supers;	/* DMA buffer size in superframes */
+	uint8_t int_sum;	/* events seen since last TDMGSTAT */
+	uint64_t rxaddr;	/* DMA offset of last Rx sample */
+	uint64_t txaddr;	/* DMA offset of last Tx sample */
+	uint64_t rxsize;	/* size of current Rx sample space */
+	uint64_t txsize;	/* size of current Tx sample space */
+	uint64_t rxlimit;	/* size of config'd Rx frame space */
+	uint64_t txlimit;	/* size of config'd Tx frame space */
 	unsigned long rxmask[TDM_MAX_SLOTS / (8 * sizeof(unsigned long))];
 	unsigned long txmask[TDM_MAX_SLOTS / (8 * sizeof(unsigned long))];
 };
 #define TDMGSTAT	_IOR(TDM_IOCTL, 0x2, struct cavium_tdm_stat)
 
 #define TDMSRXBUF	_IO(TDM_IOCTL, 0x6)	/* set rx superframe space */
-#define TDMSTXBUF	_IO(TDM_IOCTL, 0x7)	/* set rx superframe space */
+#define TDMSTXBUF	_IO(TDM_IOCTL, 0x7)	/* set tx superframe space */
 
 /*
  * TDMSBIND - bind a file descriptor to read/write slots in the TDM frame.
@@ -143,14 +132,15 @@ struct cavium_tdm_stat {
  * 'log2fifo', if set, alters the kernel<->user FIFOs which buffer DMA engines
  */
 union cavium_tdm_bind {
-	__u64 u64;
+	uint64_t u64;
 	struct {
-		__u64 slots:10;
-		__u64 rslot:10;
-		__u64 wslot:10;
-		__u64 no_rx:1;
-		__u64 no_tx:1;
-		__u64 log2fifo:4;
+		uint64_t slots:10;
+		uint64_t rslot:10;
+		uint64_t wslot:10;
+		uint64_t no_rx:1;
+		uint64_t no_tx:1;
+		uint64_t log2fifo:4;
+		uint64_t manual:1;
 	} s;
 };
 #define TDMGBIND	_IOR(TDM_IOCTL, 0x3, union cavium_tdm_bind)
@@ -158,5 +148,23 @@ union cavium_tdm_bind {
 
 #define TDMSTXRING	_IO(TDM_IOCTL, 0xc) /* set Tx DMA ring size in frames */
 #define TDMSRXRING	_IO(TDM_IOCTL, 0xd) /* set Rx DMA ring size in frames */
+#define TDMSSILENCE	_IO(TDM_IOCTL, 0xe) /* Tx underrun pattern */
+
+
+/* static slotmap, permanent tx/rx bindings */
+struct cavium_tdm_sslots {
+	unsigned long	mask[TDM_DIRS][TDM_MAX_SLOTS/sizeof(unsigned long)];
+};
+#define TDMGSSLOT	_IOR(TDM_IOCTL, 0x13, struct cavium_tdm_sslots)
+#define TDMSSSLOT	_IOW(TDM_IOCTL, 0x13, struct cavium_tdm_sslots)
+
+/*
+ * When built-in, TDM bus state persists even when no /dev/tdmX are open.
+ * If cavium-tdm is a module, it wwill normally unload on last /dev/tdmX close,
+ * but setting any state (clock, stotmap, etc) holds bus alive even when
+ * no instance open.
+ * TDMSDROP releases this hold, allowing module to unload on last close.
+ */
+#define TDMSDROP	_IO(TDM_IOCTL, 0x14)	/* release persistent state */
 
 #endif /* _UAPI_CAVM_TDM_H */
