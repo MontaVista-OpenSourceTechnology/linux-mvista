1d83b6fb07fb8de0e6e65fd264c0d979a28ba7e2
Sasha Levin <sashal@kernel.org>
2018-12-13 09:24:35 -0500

    Revert "printk: Never set console_may_schedule in console_trylock()"
    Source: Kernel.org
    MR: 96580
    Type: Security Fix
    Disposition: Backport from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-4.14.y
    ChangeID: 62582f67ff7617313b27b897ed02b569b712dcac
    Description:
    
    This reverts commit c9b8d580b3fb0ab65d37c372aef19a318fda3199.
    
    This is just a technical revert to make the printk fix apply cleanly,
    this patch will be re-picked in about 3 commits.
    
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1873,19 +1873,12 @@ asmlinkage int vprintk_emit(int facility, int level,
 			may_trylock = 0;
 #endif
 		/*
-		 * Disable preemption to avoid being preempted while holding
-		 * console_sem which would prevent anyone from printing to
-		 * console
-		 */
-		preempt_disable();
-		/*
 		 * Try to acquire and then immediately release the console
 		 * semaphore.  The release will print out buffers and wake up
 		 * /dev/kmsg and syslog() users.
 		 */
 		if (may_trylock && console_trylock())
 			console_unlock();
-		preempt_enable();
 	}
 
 	return printed_len;
@@ -2180,7 +2173,20 @@ int console_trylock(void)
 		return 0;
 	}
 	console_locked = 1;
-	console_may_schedule = 0;
+	/*
+	 * When PREEMPT_COUNT disabled we can't reliably detect if it's
+	 * safe to schedule (e.g. calling printk while holding a spin_lock),
+	 * because preempt_disable()/preempt_enable() are just barriers there
+	 * and preempt_count() is always 0.
+	 *
+	 * RCU read sections have a separate preemption counter when
+	 * PREEMPT_RCU enabled thus we must take extra care and check
+	 * rcu_preempt_depth(), otherwise RCU read sections modify
+	 * preempt_count().
+	 */
+	console_may_schedule = !oops_in_progress &&
+			preemptible() &&
+			!rcu_preempt_depth();
 	return 1;
 }
 EXPORT_SYMBOL(console_trylock);
