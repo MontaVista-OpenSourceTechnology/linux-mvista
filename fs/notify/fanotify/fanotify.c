65faa90e34053c94ddad7c4fe1825ba20858a4ee
Amir Goldstein <amir73il@gmail.com>
2020-05-27 15:54:55 +0300

    fanotify: turn off support for FAN_DIR_MODIFY
    Source: kernel.org
    MR: 412535
    Type: Defect Fix
    Disposition: Backport from v5.7-rc1
    ChangeID: f17936993af054b16725d0c54baa58115f9e052a
    Description:
    
    Dependency Patch #12
    
    FAN_DIR_MODIFY has been enabled by commit 44d705b0370b ("fanotify:
    report name info for FAN_DIR_MODIFY event") in 5.7-rc1. Now we are
    planning further extensions to the fanotify API and during that we
    realized that FAN_DIR_MODIFY may behave slightly differently to be more
    consistent with extensions we plan. So until we finalize these
    extensions, let's not bind our hands with exposing FAN_DIR_MODIFY to
    userland.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- fs/notify/fanotify/fanotify.c
+++ fs/notify/fanotify/fanotify.c
@@ -534,7 +534,7 @@ static int fanotify_handle_event(struct fsnotify_group *group, u32 mask,
 	BUILD_BUG_ON(FAN_OPEN_EXEC != FS_OPEN_EXEC);
 	BUILD_BUG_ON(FAN_OPEN_EXEC_PERM != FS_OPEN_EXEC_PERM);
 
-	BUILD_BUG_ON(HWEIGHT32(ALL_FANOTIFY_EVENT_BITS) != 20);
+	BUILD_BUG_ON(HWEIGHT32(ALL_FANOTIFY_EVENT_BITS) != 19);
 
 	mask = fanotify_group_event_mask(group, iter_info, mask, data,
 					 data_type);
