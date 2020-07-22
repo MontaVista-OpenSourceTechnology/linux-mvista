048c2572d775db2a6a2215222c9ecc5fd541a478
Amir Goldstein <amir73il@gmail.com>
2020-07-22 15:58:48 +0300

    fsnotify: create method handle_inode_event() in fsnotify_operations
    Source: kernel.org
    MR: 355430
    Type: Defect Fix
    Disposition: Backport from v5.9-rc1
    ChangeID: b9a1b9772509cbc6f6aa8bcd0b019f6347a2b631
    Description:
    
    The method handle_event() grew a lot of complexity due to the design of
    fanotify and merging of ignore masks.
    
    Most backends do not care about this complex functionality, so we can hide
    this complexity from them.
    
    Introduce a method handle_inode_event() that serves those backends and
    passes a single inode mark and less arguments.
    
    This change converts all backends except fanotify and inotify to use the
    simplified handle_inode_event() method.  In pricipal, inotify could have
    also used the new method, but that would require passing more arguments
    on the simple helper (data, data_type, cookie), so we leave it with the
    handle_event() method.
    
    Link: https://lore.kernel.org/r/20200722125849.17418-9-amir73il@gmail.com
    Suggested-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- fs/nfsd/filecache.c
+++ fs/nfsd/filecache.c
@@ -604,11 +604,9 @@ static struct notifier_block nfsd_file_lease_notifier = {
 };
 
 static int
-nfsd_file_fsnotify_handle_event(struct fsnotify_group *group, u32 mask,
-				const void *data, int data_type,
-				struct inode *dir,
-				const struct qstr *file_name, u32 cookie,
-				struct fsnotify_iter_info *iter_info)
+nfsd_file_fsnotify_handle_event(struct fsnotify_mark *mark, u32 mask,
+				struct inode *inode, struct inode *dir,
+				const struct qstr *name)
 {
 	trace_nfsd_file_fsnotify_handle_event(inode, mask);
 
@@ -630,7 +628,7 @@ nfsd_file_fsnotify_handle_event(struct fsnotify_group *group, u32 mask,
 
 
 static const struct fsnotify_ops nfsd_file_fsnotify_ops = {
-	.handle_event = nfsd_file_fsnotify_handle_event,
+	.handle_inode_event = nfsd_file_fsnotify_handle_event,
 	.free_mark = nfsd_file_mark_free,
 };
 
