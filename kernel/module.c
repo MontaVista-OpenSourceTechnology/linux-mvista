a359cea5a4d788d15ac47dbc619ce3d58debdbfa
Christoph Hellwig <hch@lst.de>
2020-07-30 08:10:24 +0200

    modules: unexport __module_address
    Source: Kernel.org
    MR: 112708
    Type: Integration
    Disposition: Backport from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-4.14.y
    ChangeID: 5160c8c806400994ad2e91a3884173061fbcfd12
    Description:
    
    commit 34e64705ad415ed7a816e60ef62b42fe6d1729d9 upstream.
    
    __module_address is only used by built-in code.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jessica Yu <jeyu@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- kernel/module.c
+++ kernel/module.c
@@ -4340,7 +4340,6 @@ struct module *__module_address(unsigned long addr)
 	}
 	return mod;
 }
-EXPORT_SYMBOL_GPL(__module_address);
 
 /*
  * is_module_text_address - is this address inside module code?
