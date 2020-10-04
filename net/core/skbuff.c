b3f38443e47c1a417af6d16636f0d543a115ff94
Dushyanth Ramesh <dramesh@mvista.com>
2020-10-04 14:29:52 +0000

    AGIB : Add mstamp & alloc_head for Rionet skbuff
    Source: mvista_shared_12_10_2020.tar
    MR: 105937
    Type: Integration
    Disposition: Merged from mvista_shared_12_10_2020.tar
    ChangeID: ce43705202e79ecce7324a6d41ffdb8e26807062
    Description:
    
    Add multi resolution time stamps  and __alloc_skb_head
    support.
    
    Signed-off-by: Dushyanth Ramesh <dramesh@mvista.com>
    Acked-by: Corey Minyard <cminyard@mvista.com>
    Signed-off-by: Niyas Ahamed Mydeen <nmydeen@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -158,6 +158,33 @@ out:
  *
  */
 
+struct sk_buff *__alloc_skb_head(gfp_t gfp_mask, int node)
+{
+       struct sk_buff *skb;
+
+       /* Get the HEAD */
+       skb = kmem_cache_alloc_node(skbuff_head_cache,
+                                   gfp_mask & ~__GFP_DMA, node);
+       if (!skb)
+               goto out;
+
+       /*
+        * Only clear those fields we need to clear, not those that we will
+        * actually initialise below. Hence, don't put any more fields after
+        * the tail pointer in struct sk_buff!
+        */
+       memset(skb, 0, offsetof(struct sk_buff, tail));
+       skb->head = NULL;
+       skb->truesize = sizeof(struct sk_buff);
+       refcount_set(&skb->users, 1);
+
+       skb->mac_header = (typeof(skb->mac_header))~0U;
+out:
+       return skb;
+}
+EXPORT_SYMBOL_GPL(__alloc_skb_head);
+
+
 /**
  *	__alloc_skb	-	allocate a network buffer
  *	@size: size to allocate
