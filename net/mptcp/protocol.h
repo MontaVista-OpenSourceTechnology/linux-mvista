2237746f7000124d4a27ffa3008178398e8e5ebe
Paolo Abeni <pabeni@redhat.com>
2020-11-19 11:46:03 -0800

    mptcp: refine MPTCP-level ack scheduling
    Source: https://git.kernel.org
    MR: 199386
    Type: Security Fix
    Disposition: Backport from v5.11-rc1
    ChangeID: ea4ca586b16ff2eb6157fe13969eb72d2403a3a1
    Description:
    
    CVE-2024-53122 kernel: mptcp: cope racing subflow creation
    in mptcp_rcv_space_adjust
    
    Dependency Patch #13.
    
    Send timely MPTCP-level ack is somewhat difficult when
    the insertion into the msk receive level is performed
    by the worker.
    
    It needs TCP-level dup-ack to notify the MPTCP-level
    ack_seq increase, as both the TCP-level ack seq and the
    rcv window are unchanged.
    
    We can actually avoid processing incoming data with the
    worker, and let the subflow or recevmsg() send ack as needed.
    
    When recvmsg() moves the skbs inside the msk receive queue,
    the msk space is still unchanged, so tcp_cleanup_rbuf() could
    end-up skipping TCP-level ack generation. Anyway, when
    __mptcp_move_skbs() is invoked, a known amount of bytes is
    going to be consumed soon: we update rcv wnd computation taking
    them in account.
    
    Additionally we need to explicitly trigger tcp_cleanup_rbuf()
    when recvmsg() consumes a significant amount of the receive buffer.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -214,9 +214,11 @@ struct mptcp_sock {
 	u64		rcv_data_fin_seq;
 	struct sock	*last_snd;
 	int		snd_burst;
+	int		old_wspace;
 	atomic64_t	snd_una;
 	unsigned long	timer_ival;
 	u32		token;
+	int		rmem_pending;
 	unsigned long	flags;
 	bool		can_ack;
 	bool		fully_established;
@@ -225,6 +227,7 @@ struct mptcp_sock {
 	bool		use_64bit_ack; /* Set when we received a 64-bit DSN */
 	bool		allow_infinite_fallback;
 	spinlock_t	join_list_lock;
+	struct sock	*ack_hint;
 	struct work_struct work;
 	struct sk_buff  *ooo_last_skb;
 	struct rb_root  out_of_order_queue;
@@ -250,6 +253,11 @@ struct mptcp_sock {
 static inline struct mptcp_sock *mptcp_sk(const struct sock *sk)
 {
 	return (struct mptcp_sock *)sk;
+}
+
+static inline int __mptcp_space(const struct sock *sk)
+{
+	return tcp_space(sk) + READ_ONCE(mptcp_sk(sk)->rmem_pending);
 }
 
 static inline struct mptcp_data_frag *mptcp_send_head(const struct sock *sk)
