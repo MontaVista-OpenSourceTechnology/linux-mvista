ae67181953873c9cba8f4ea40197aa6128129593
Alexander Lobakin <alobakin@pm.me>
2021-01-22 18:19:48 +0000

    net: introduce a netdev feature for UDP GRO forwarding
    Source: https://git.kernel.org/
    MR: 141033
    Type: Security Fix
    Disposition: Backport from v5.11-rc4-594-g6f1c0ea133a6
    ChangeID: 6f1c0ea133a6e4a193a7b285efe209664caeea43
    Description:
    
    CVE-2021-47036 kernel: udp: skip L4 aggregation for UDP tunnel packets
    
    Manual change was done in this patch to incorporate changes added in file
    include/linux/netdev_features.h by commit aedf18ce2136bf9e25a5a4453151c268b616e009
    in mvl-5.10/msd.cgx
    
    Dependency patch #1
    
    Introduce a new netdev feature, NETIF_F_GRO_UDP_FWD, to allow user
    to turn UDP GRO on and off for forwarding.
    Defaults to off to not change current datapath.
    
    Suggested-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Alexander Lobakin <alobakin@pm.me>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Shubham Kulkarni <skulkarni@mvista.com>
    Reviewed-by: Corey Minyard <cminyard@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/linux/netdev_features.h
+++ include/linux/netdev_features.h
@@ -85,6 +85,7 @@ enum {
 	NETIF_F_GRO_FRAGLIST_BIT,	/* Fraglist GRO */
 
 	NETIF_F_HW_MACSEC_BIT,		/* Offload MACsec operations */
+	NETIF_F_GRO_UDP_FWD_BIT,	/* Allow UDP GRO for forwarding */
 
 	/*
 	 * Add your fresh new feature above and remember to update
@@ -159,6 +160,7 @@ enum {
 #define NETIF_F_GRO_FRAGLIST	__NETIF_F(GRO_FRAGLIST)
 #define NETIF_F_GSO_FRAGLIST	__NETIF_F(GSO_FRAGLIST)
 #define NETIF_F_HW_MACSEC	__NETIF_F(HW_MACSEC)
+#define NETIF_F_GRO_UDP_FWD	__NETIF_F(GRO_UDP_FWD)
 
 /* Finds the next feature with the highest number of the range of start-1 till 0.
  */
@@ -236,7 +238,7 @@ static inline int find_next_netdev_feature(u64 feature, unsigned long start)
 #define NETIF_F_SOFT_FEATURES	(NETIF_F_GSO | NETIF_F_GRO)
 
 /* Changeable features with no special hardware requirements that defaults to off. */
-#define NETIF_F_SOFT_FEATURES_OFF	NETIF_F_GRO_FRAGLIST
+#define NETIF_F_SOFT_FEATURES_OFF	(NETIF_F_GRO_FRAGLIST | NETIF_F_GRO_UDP_FWD)
 
 #define NETIF_F_VLAN_FEATURES	(NETIF_F_HW_VLAN_CTAG_FILTER | \
 				 NETIF_F_HW_VLAN_CTAG_RX | \
