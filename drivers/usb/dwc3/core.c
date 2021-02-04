20d24e456029fffe37cda0e262d9444efab58c9e
Manish Narani <manish.narani@xilinx.com>
2021-02-04 11:49:28 -0700

    usb: dwc3: core: Add regulator calls for communicating with dwc3 PMU
    Source: Xilinx
    MR: 117648
    Type: Integration
    Disposition: Merged from https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.10
    ChangeID: 891dc833fd265874ae5b4769081a7b3fa8d6f552
    Description:
    
    This patch adds regulator calls for requesting the dwc3 PMU (Power
    Management Unit) module to put the core into D3/D0 state.
    
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    State: pending
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1692,12 +1692,26 @@ static int dwc3_probe(struct platform_device *pdev)
 	}
 
 	dwc3_check_params(dwc);
-	dwc3_debugfs_init(dwc);
 
 	ret = dwc3_core_init_mode(dwc);
 	if (ret)
 		goto err5;
 
+	/*
+	 * DWC3 controller has a Power Management Unit(PMU) module
+	 * which requests the power controller for entering into
+	 * D3/D0 state. Try getting the regulator.
+	 */
+	dwc->dwc3_pmu = devm_regulator_get(dev, "dwc3-pmu-regulator");
+	if (!IS_ERR(dwc->dwc3_pmu)) {
+		ret = regulator_enable(dwc->dwc3_pmu);
+		if (ret) {
+			dev_err(dev, "Failed to enable dwc3_pmu supply\n");
+			goto err5;
+		}
+	}
+
+	dwc3_debugfs_init(dwc);
 	pm_runtime_put(dev);
 
 	return 0;
@@ -1760,6 +1774,9 @@ static int dwc3_remove(struct platform_device *pdev)
 
 	dwc3_core_exit(dwc);
 	dwc3_ulpi_exit(dwc);
+
+	if (dwc->dwc3_pmu)
+		regulator_disable(dwc->dwc3_pmu);
 
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_put_noidle(&pdev->dev);
@@ -1874,6 +1891,15 @@ static int dwc3_resume_common(struct dwc3 *dwc, pm_message_t msg)
 	unsigned long	flags;
 	int		ret;
 	u32		reg;
+
+	/* Bring core to D0 state */
+	if (dwc->dwc3_pmu) {
+		ret = regulator_enable(dwc->dwc3_pmu);
+		if (ret) {
+			dev_err(dwc->dev, "Failed to enable dwc3_pmu supply\n");
+			return ret;
+		}
+	}
 
 	switch (dwc->current_dr_role) {
 	case DWC3_GCTL_PRTCAP_DEVICE:
