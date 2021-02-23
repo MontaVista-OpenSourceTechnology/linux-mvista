3f2c272d1e45e385a4092f668f645bc479ab51fa
Cong Wang <cong.wang@bytedance.com>
2021-02-23 10:49:27 -0800

    skmsg: Get rid of struct sk_psock_parser
    Source: https://git.kernel.org
    MR: 439575
    Type: Security Fix
    Disposition: Backport from v5.13-rc1
    ChangeID: 5a685cd94b21a88efa6be77169eddef525368034
    Description:
    
    CVE-2025-68188 kernel: tcp: use dst_dev_rcu() in
    tcp_fastopen_active_disable_ofo_check()
    
    Dependency Patch #1.
    
    struct sk_psock_parser is embedded in sk_psock, it is
    unnecessary as skb verdict also uses ->saved_data_ready.
    We can simply fold these fields into sk_psock, and get rid
    of ->enabled.
    
    Signed-off-by: Cong Wang <cong.wang@bytedance.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Jakub Sitnicki <jakub@cloudflare.com>
    Link: https://lore.kernel.org/bpf/20210223184934.6054-3-xiyou.wangcong@gmail.com
    
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/linux/skmsg.h
+++ include/linux/skmsg.h
@@ -70,14 +70,6 @@ struct sk_psock_link {
 	void				*link_raw;
 };
 
-struct sk_psock_parser {
-#if IS_ENABLED(CONFIG_BPF_STREAM_PARSER)
-	struct strparser		strp;
-#endif
-	bool				enabled;
-	void (*saved_data_ready)(struct sock *sk);
-};
-
 struct sk_psock_work_state {
 	struct sk_buff			*skb;
 	u32				len;
@@ -92,7 +84,9 @@ struct sk_psock {
 	u32				eval;
 	struct sk_msg			*cork;
 	struct sk_psock_progs		progs;
-	struct sk_psock_parser		parser;
+#if IS_ENABLED(CONFIG_BPF_STREAM_PARSER)
+	struct strparser		strp;
+#endif
 	struct sk_buff_head		ingress_skb;
 	struct list_head		ingress_msg;
 	spinlock_t			ingress_lock;
@@ -104,6 +98,7 @@ struct sk_psock {
 	void (*saved_destroy)(struct sock *sk);
 	void (*saved_close)(struct sock *sk, long timeout);
 	void (*saved_write_space)(struct sock *sk);
+	void (*saved_data_ready)(struct sock *sk);
 	int  (*psock_update_sk_prot)(struct sock *sk, struct sk_psock *psock,
 				     bool restore);
 	struct proto			*sk_proto;
@@ -459,8 +454,8 @@ static inline void sk_psock_put(struct sock *sk, struct sk_psock *psock)
 static inline void sk_psock_data_ready(struct sock *sk, struct sk_psock *psock)
 {
 	read_lock_bh(&sk->sk_callback_lock);
-	if (psock->parser.enabled)
-		psock->parser.saved_data_ready(sk);
+	if (psock->saved_data_ready)
+		psock->saved_data_ready(sk);
 	else
 		sk->sk_data_ready(sk);
 	read_unlock_bh(&sk->sk_callback_lock);
@@ -500,6 +495,6 @@ static inline bool sk_psock_strp_enabled(struct sk_psock *psock)
 {
 	if (!psock)
 		return false;
-	return psock->parser.enabled;
+	return !!psock->saved_data_ready;
 }
 #endif /* _LINUX_SKMSG_H */
