1ee91a7f8fccff2327d48fe8c520f09b9ec6fd64
Pranavi Somisetty <pranavi.somisetty@xilinx.com>
2021-03-22 06:04:42 -0600

    net: macb: Fix Wake on Lan failure on 5.10 kernel branch
    Source: Xilinx
    MR: 117648
    Type: Integration
    Disposition: Merged from https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.10
    ChangeID: 788da64580bbb6086f1a343b7ee6efb531e0df7e
    Description:
    
    Wake on Lan feature is broken on 5.10 version, due to:
    1. Mainline kernel uses, struct dev to set/check if a device is
    wakeup capable. However Xilinx's implementation of wake on Lan
    uses bp->dev->dev. Replace "dev" with "&bp->dev->dev" in
    macb_runtime_suspend/resume so that macb's clocks won't be
    disabled when its selected as wake device.
    2. After the merge, napi_disable, netif_device_detach in macb_suspend
    and napi_enable, netif_carrier_on in macb_resume, are being called
    twice (when macb is selected as wake device), resulting in a hang
    during suspend and a crash during resume. Removed one call each of
    the above functions.
    
    Signed-off-by: Pranavi Somisetty <pranavi.somisetty@xilinx.com>
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    State: pending
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -4800,10 +4800,6 @@ static int __maybe_unused macb_suspend(struct device *dev)
 		gem_writel(bp, WOL, MACB_BIT(ARP) | arpipmask);
 		spin_unlock_irqrestore(&bp->lock, flags);
 		enable_irq_wake(bp->queues[0].irq);
-		netif_device_detach(netdev);
-		for (q = 0, queue = bp->queues; q < bp->num_queues;
-		     ++q, ++queue)
-			napi_disable(&queue->napi);
 	}
 
 	netif_device_detach(netdev);
@@ -4828,7 +4824,7 @@ static int __maybe_unused macb_suspend(struct device *dev)
 
 	if (bp->ptp_info)
 		bp->ptp_info->ptp_remove(netdev);
-	if (!device_may_wakeup(dev))
+	if (!device_may_wakeup(&bp->dev->dev))
 		pm_runtime_force_suspend(dev);
 
 	return 0;
@@ -4845,7 +4841,7 @@ static int __maybe_unused macb_resume(struct device *dev)
 	if (!netif_running(netdev))
 		return 0;
 
-	if (!device_may_wakeup(dev))
+	if (!device_may_wakeup(&bp->dev->dev))
 		pm_runtime_force_resume(dev);
 
 	if (device_may_wakeup(&bp->dev->dev)) {
@@ -4858,10 +4854,6 @@ static int __maybe_unused macb_resume(struct device *dev)
 		disable_irq_wake(bp->queues[0].irq);
 		spin_unlock_irqrestore(&bp->lock, flags);
 		macb_writel(bp, NCR, MACB_BIT(MPE));
-		for (q = 0, queue = bp->queues; q < bp->num_queues;
-		     ++q, ++queue)
-			napi_enable(&queue->napi);
-		netif_carrier_on(netdev);
 	}
 
 	for (q = 0, queue = bp->queues; q < bp->num_queues;
@@ -4894,7 +4886,7 @@ static int __maybe_unused macb_runtime_suspend(struct device *dev)
 	struct net_device *netdev = dev_get_drvdata(dev);
 	struct macb *bp = netdev_priv(netdev);
 
-	if (!(device_may_wakeup(dev))) {
+	if (!(device_may_wakeup(&bp->dev->dev))) {
 		clk_disable_unprepare(bp->tx_clk);
 		clk_disable_unprepare(bp->hclk);
 		clk_disable_unprepare(bp->pclk);
@@ -4913,7 +4905,7 @@ static int __maybe_unused macb_runtime_resume(struct device *dev)
 	struct net_device *netdev = dev_get_drvdata(dev);
 	struct macb *bp = netdev_priv(netdev);
 
-	if (!(device_may_wakeup(dev))) {
+	if (!(device_may_wakeup(&bp->dev->dev))) {
 		clk_prepare_enable(bp->pclk);
 		clk_prepare_enable(bp->hclk);
 		clk_prepare_enable(bp->tx_clk);
