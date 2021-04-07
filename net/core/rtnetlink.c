aff96eb42375ac06140d2c032ec3e33270c0bd86
Andrei Vagin <avagin@gmail.com>
2021-04-06 23:40:51 -0700

    net: remove the new_ifindex argument from dev_change_net_namespace
    Source: https://git.kernel.org
    MR: 438988
    Type: Security Fix
    Disposition: Backport from v5.13-rc1
    ChangeID: 0854fa82c96ca37a35e954b7079c0bfd795affb1
    Description:
    
    CVE-2026-43042 kernel: mpls: add seqcount to protect the platform_label{,s}
    pair
    
    Dependency Patch #2.
    
    Here is only one place where we want to specify new_ifindex. In all
    other cases, callers pass 0 as new_ifindex. It looks reasonable to add a
    low-level function with new_ifindex and to convert
    dev_change_net_namespace to a static inline wrapper.
    
    Fixes: eeb85a14ee34 ("net: Allow to specify ifindex when device is moved to another namespace")
    Suggested-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -2659,7 +2659,7 @@ static int do_setlink(const struct sk_buff *skb,
 		else
 			new_ifindex = 0;
 
-		err = dev_change_net_namespace(dev, net, pat, new_ifindex);
+		err = __dev_change_net_namespace(dev, net, pat, new_ifindex);
 		put_net(net);
 		if (err)
 			goto errout;
@@ -3516,7 +3516,7 @@ replay:
 	if (err < 0)
 		goto out_unregister;
 	if (link_net) {
-		err = dev_change_net_namespace(dev, dest_net, ifname, 0);
+		err = dev_change_net_namespace(dev, dest_net, ifname);
 		if (err < 0)
 			goto out_unregister;
 	}
