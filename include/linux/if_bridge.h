19924df46697da281f5f85f276aec3dc77ea26c5
Linus Lüssing <linus.luessing@c0d3.blue>
2021-05-13 15:20:53 +0200

    net: bridge: mcast: export multicast router presence adjacent to a port
    Source: https://git.kernel.org
    MR: 162926
    Type: Security Fix
    Disposition: Backport from v5.14-rc1
    ChangeID: 3b85f9ba3480c1bcbebb2bb490822bec0e7a1201
    Description:
    
    CVE-2024-44934 kernel: net: bridge: mcast: wait for previous gc cycles
    when removing port
    
    Dependency Patch #19.
    
    To properly support routable multicast addresses in batman-adv in a
    group-aware way, a batman-adv node needs to know if it serves multicast
    routers.
    
    This adds a function to the bridge to export this so that batman-adv
    can then make full use of the Multicast Router Discovery capability of
    the bridge.
    
    Signed-off-by: Linus Lüssing <linus.luessing@c0d3.blue>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/linux/if_bridge.h
+++ include/linux/if_bridge.h
@@ -66,6 +66,7 @@ int br_multicast_list_adjacent(struct net_device *dev,
 			       struct list_head *br_ip_list);
 bool br_multicast_has_querier_anywhere(struct net_device *dev, int proto);
 bool br_multicast_has_querier_adjacent(struct net_device *dev, int proto);
+bool br_multicast_has_router_adjacent(struct net_device *dev, int proto);
 bool br_multicast_enabled(const struct net_device *dev);
 bool br_multicast_router(const struct net_device *dev);
 #else
@@ -84,6 +85,13 @@ static inline bool br_multicast_has_querier_adjacent(struct net_device *dev,
 {
 	return false;
 }
+
+static inline bool br_multicast_has_router_adjacent(struct net_device *dev,
+						    int proto)
+{
+	return true;
+}
+
 static inline bool br_multicast_enabled(const struct net_device *dev)
 {
 	return false;
