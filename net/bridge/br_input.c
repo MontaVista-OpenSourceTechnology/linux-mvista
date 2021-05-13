3fa317dc387f43d07e7e91f749db82e5ed6a196c
Linus Lüssing <linus.luessing@c0d3.blue>
2021-05-13 15:20:47 +0200

    net: bridge: mcast: prepare is-router function for mcast router split
    Source: https://git.kernel.org
    MR: 162926
    Type: Security Fix
    Disposition: Backport from v5.14-rc1
    ChangeID: 1a3065a26807b4cdd65d3b696ddb18385610f7da
    Description:
    
    CVE-2024-44934 kernel: net: bridge: mcast: wait for previous gc cycles
    when removing port
    
    Dependency Patch #12.
    
    In preparation for the upcoming split of multicast router state into
    their IPv4 and IPv6 variants make br_multicast_is_router() protocol
    family aware.
    
    Note that for now br_ip6_multicast_is_router() uses the currently still
    common ip4_mc_router_timer for now. It will be renamed to
    ip6_mc_router_timer later when the split is performed.
    
    While at it also renames the "1" and "2" constants in
    br_multicast_is_router() to the MDB_RTR_TYPE_TEMP_QUERY and
    MDB_RTR_TYPE_PERM enums.
    
    Signed-off-by: Linus Lüssing <linus.luessing@c0d3.blue>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- net/bridge/br_input.c
+++ net/bridge/br_input.c
@@ -148,7 +148,7 @@ int br_handle_frame_finish(struct net *net, struct sock *sk, struct sk_buff *skb
 		if ((mdst || BR_INPUT_SKB_CB_MROUTERS_ONLY(skb)) &&
 		    br_multicast_querier_exists(br, eth_hdr(skb))) {
 			if ((mdst && mdst->host_joined) ||
-			    br_multicast_is_router(br)) {
+			    br_multicast_is_router(br, skb)) {
 				local_rcv = true;
 				DEV_STATS_INC(br->dev, multicast);
 			}
