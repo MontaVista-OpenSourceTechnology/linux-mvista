a3b5ffb706969d849b57eb3019e697f5fde16d67
Jing Zhang <jingzhangos@google.com>
2021-06-18 22:27:03 +0000

    KVM: stats: Separate generic stats from architecture specific ones
    Source: https://git.kernel.org
    MR: 122158
    Type: Security Fix
    Disposition: Backport from v5.14-rc2
    ChangeID: 0193cc908b5ae8aff2e2d2997ca5d4ae26ed24d4
    Description:
    
    CVE-2022-39189 Kernel: TLB flush operations are mishandled in certain
     KVM_VCPU_PREEMPTED leading to guest malfunctioning
    
    Dependency Patch #6.
    
    Generic KVM stats are those collected in architecture independent code
    or those supported by all architectures; put all generic statistics in
    a separate structure.  This ensures that they are defined the same way
    in the statistics API which is being added, removing duplication among
    different architectures in the declaration of the descriptors.
    
    No functional change intended.
    
    Reviewed-by: David Matlack <dmatlack@google.com>
    Reviewed-by: Ricardo Koller <ricarkol@google.com>
    Reviewed-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    Signed-off-by: Jing Zhang <jingzhangos@google.com>
    Message-Id: <20210618222709.1858088-2-jingzhangos@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- arch/mips/kvm/mips.c
+++ arch/mips/kvm/mips.c
@@ -71,12 +71,6 @@ struct kvm_stats_debugfs_item debugfs_entries[] = {
 	VCPU_STAT("vz_cpucfg", vz_cpucfg_exits),
 #endif
 #endif
-	VCPU_STAT("halt_successful_poll", halt_successful_poll),
-	VCPU_STAT("halt_attempted_poll", halt_attempted_poll),
-	VCPU_STAT("halt_poll_invalid", halt_poll_invalid),
-	VCPU_STAT("halt_wakeup", halt_wakeup),
-	VCPU_STAT("halt_poll_success_ns", halt_poll_success_ns),
-	VCPU_STAT("halt_poll_fail_ns", halt_poll_fail_ns),
 	{NULL}
 };
 
