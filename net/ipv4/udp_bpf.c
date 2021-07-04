d80cedeaee81e60ef4872b8de49624b839a3a7ad
Cong Wang <cong.wang@bytedance.com>
2021-07-04 12:02:43 -0700

    sock_map: Lift socket state restriction for datagram sockets
    Source: https://git.kernel.org
    MR: 439575
    Type: Security Fix
    Disposition: Backport from v5.15-rc1
    ChangeID: 0c48eefae712c2fd91480346a07a1a9cd0f9470b
    Description:
    
    CVE-2025-68188 kernel: tcp: use dst_dev_rcu() in
    tcp_fastopen_active_disable_ofo_check()
    
    Dependency Patch #7.
    
    TCP and other connection oriented sockets have accept()
    for each incoming connection on the server side, hence
    they can just insert those fd's from accept() to sockmap,
    which are of course established.
    
    Now with datagram sockets begin to support sockmap and
    redirection, the restriction is no longer applicable to
    them, as they have no accept(). So we have to lift this
    restriction for them. This is fine, because inside
    bpf_sk_redirect_map() we still have another socket status
    check, sock_map_redirect_allowed(), as a guard.
    
    This also means they do not have to be removed from
    sockmap when disconnecting.
    
    Signed-off-by: Cong Wang <cong.wang@bytedance.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20210704190252.11866-3-xiyou.wangcong@gmail.com
    
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- net/ipv4/udp_bpf.c
+++ net/ipv4/udp_bpf.c
@@ -18,7 +18,6 @@ static struct proto udp_bpf_prots[UDP_BPF_NUM_PROTS];
 static void udp_bpf_rebuild_protos(struct proto *prot, const struct proto *base)
 {
 	*prot        = *base;
-	prot->unhash = sock_map_unhash;
 	prot->close  = sock_map_close;
 }
 
