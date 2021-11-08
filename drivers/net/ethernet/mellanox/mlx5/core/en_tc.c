937bf4928e4bbc4e2763ecd1b7f51591264e0587
Roi Dayan <roid@nvidia.com>
2021-11-08 16:41:05 +0200

    net/mlx5e: CT, Fix multiple allocations and memleak of mod acts
    Source: https://git.kernel.org/
    MR: 137691
    Type: Security Fix
    Disposition: Backport from v5.15.5~132
    ChangeID: 486e8de6e233ff2999493533c6259d1cb538653b
    Description:
    
    CVE-2021-47199 kernel: net/mlx5e: CT, Fix multiple allocations and memleak of mod acts
    
    [ Upstream commit 806401c20a0f9c51b6c8fd7035671e6ca841f6c2 ]
    
    CT clear action offload adds additional mod hdr actions to the
    flow's original mod actions in order to clear the registers which
    hold ct_state.
    When such flow also includes encap action, a neigh update event
    can cause the driver to unoffload the flow and then reoffload it.
    
    Each time this happens, the ct clear handling adds that same set
    of mod hdr actions to reset ct_state until the max of mod hdr
    actions is reached.
    
    Also the driver never releases the allocated mod hdr actions and
    causing a memleak.
    
    Fix above two issues by moving CT clear mod acts allocation
    into the parsing actions phase and only use it when offloading the rule.
    The release of mod acts will be done in the normal flow_put().
    
     backtrace:
        [<000000007316e2f3>] krealloc+0x83/0xd0
        [<00000000ef157de1>] mlx5e_mod_hdr_alloc+0x147/0x300 [mlx5_core]
        [<00000000970ce4ae>] mlx5e_tc_match_to_reg_set_and_get_id+0xd7/0x240 [mlx5_core]
        [<0000000067c5fa17>] mlx5e_tc_match_to_reg_set+0xa/0x20 [mlx5_core]
        [<00000000d032eb98>] mlx5_tc_ct_entry_set_registers.isra.0+0x36/0xc0 [mlx5_core]
        [<00000000fd23b869>] mlx5_tc_ct_flow_offload+0x272/0x1f10 [mlx5_core]
        [<000000004fc24acc>] mlx5e_tc_offload_fdb_rules.part.0+0x150/0x620 [mlx5_core]
        [<00000000dc741c17>] mlx5e_tc_encap_flows_add+0x489/0x690 [mlx5_core]
        [<00000000e92e49d7>] mlx5e_rep_update_flows+0x6e4/0x9b0 [mlx5_core]
        [<00000000f60f5602>] mlx5e_rep_neigh_update+0x39a/0x5d0 [mlx5_core]
    
    Fixes: 1ef3018f5af3 ("net/mlx5e: CT: Support clear action")
    Signed-off-by: Roi Dayan <roid@nvidia.com>
    Reviewed-by: Paul Blakey <paulb@nvidia.com>
    Reviewed-by: Maor Dickman <maord@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: E V Ravi <reluri@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -3655,7 +3655,9 @@ static int parse_tc_nic_actions(struct mlx5e_priv *priv,
 			attr->dest_chain = act->chain_index;
 			break;
 		case FLOW_ACTION_CT:
-			err = mlx5_tc_ct_parse_action(get_ct_priv(priv), attr, act, extack);
+			err = mlx5_tc_ct_parse_action(get_ct_priv(priv), attr,
+						      &parse_attr->mod_hdr_acts,
+						      act, extack);
 			if (err)
 				return err;
 
@@ -4495,7 +4497,9 @@ static int parse_tc_fdb_actions(struct mlx5e_priv *priv,
 			attr->dest_chain = act->chain_index;
 			break;
 		case FLOW_ACTION_CT:
-			err = mlx5_tc_ct_parse_action(get_ct_priv(priv), attr, act, extack);
+			err = mlx5_tc_ct_parse_action(get_ct_priv(priv), attr,
+							&parse_attr->mod_hdr_acts,
+							act, extack);
 			if (err)
 				return err;
 
