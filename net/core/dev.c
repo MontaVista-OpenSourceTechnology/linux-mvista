5b8bf47eb336efbecb7fbbd09f0418316d4a13e5
Eric Dumazet <edumazet@google.com>
2021-11-19 07:43:32 -0800

    net: annotate accesses to dev->gso_max_segs
    Source: https://git.kernel.org
    MR: 439575
    Type: Security Fix
    Disposition: Backport from v5.17-rc1
    ChangeID: 6d872df3e3b91532b142de9044e5b4984017a55f
    Description:
    
    CVE-2025-68188 kernel: tcp: use dst_dev_rcu() in
    tcp_fastopen_active_disable_ofo_check()
    
    Dependency Patch #10.
    
    dev->gso_max_segs is written under RTNL protection, or when the device is
    not yet visible, but is read locklessly.
    
    Add netif_set_gso_max_segs() helper.
    
    Add the READ_ONCE()/WRITE_ONCE() pairs, and use netif_set_gso_max_segs()
    where we can to better document what is going on.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3523,7 +3523,7 @@ static netdev_features_t gso_features_check(const struct sk_buff *skb,
 {
 	u16 gso_segs = skb_shinfo(skb)->gso_segs;
 
-	if (gso_segs > dev->gso_max_segs)
+	if (gso_segs > READ_ONCE(dev->gso_max_segs))
 		return features & ~NETIF_F_GSO_MASK;
 
 	if (unlikely(skb->len >= READ_ONCE(dev->gso_max_size)))
