08064f6c6e0b006fac01fb55a9e352d4f536970d
Christoph Hellwig <hch@lst.de>
2022-02-26 16:40:21 +0100

    dma-mapping: remove CONFIG_DMA_REMAP
    Source: https://git.kernel.org
    MR: 217975
    Type: Security Fix
    Disposition: Backport from v5.18-rc1
    ChangeID: f5ff79fddf0efecca538046b5cc20fb3ded2ec4f
    Description:
    
    CVE-2024-35939 kernel: dma-direct: Leak pages on dma_set_decrypted() failure
    
    Dependency Patch #8.
    
    CONFIG_DMA_REMAP is used to build a few helpers around the core
    vmalloc code, and to use them in case there is a highmem page in
    dma-direct, and to make dma coherent allocations be able to use
    non-contiguous pages allocations for DMA allocations in the dma-iommu
    layer.
    
    Right now it needs to be explicitly selected by architectures, and
    is only done so by architectures that require remapping to deal
    with devices that are not DMA coherent.  Make it unconditional for
    builds with CONFIG_MMU as it is very little extra code, but makes
    it much more likely that large DMA allocations succeed on x86.
    
    This fixes hot plugging a NVMe thunderbolt SSD for me, which tries
    to allocate a 1MB buffer that is otherwise hard to obtain due to
    memory fragmentation on a heavily used laptop.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- drivers/iommu/dma-iommu.c
+++ drivers/iommu/dma-iommu.c
@@ -957,7 +957,7 @@ static void __iommu_dma_free(struct device *dev, size_t size, void *cpu_addr)
 	    dma_free_from_pool(dev, cpu_addr, alloc_size))
 		return;
 
-	if (IS_ENABLED(CONFIG_DMA_REMAP) && is_vmalloc_addr(cpu_addr)) {
+	if (is_vmalloc_addr(cpu_addr)) {
 		/*
 		 * If it the address is remapped, then it's either non-coherent
 		 * or highmem CMA, or an iommu_dma_alloc_remap() construction.
@@ -999,7 +999,7 @@ static void *iommu_dma_alloc_pages(struct device *dev, size_t size,
 	if (!page)
 		return NULL;
 
-	if (IS_ENABLED(CONFIG_DMA_REMAP) && (!coherent || PageHighMem(page))) {
+	if (!coherent || PageHighMem(page)) {
 		pgprot_t prot = dma_pgprot(dev, PAGE_KERNEL, attrs);
 
 		cpu_addr = dma_common_contiguous_remap(page, alloc_size,
@@ -1031,7 +1031,7 @@ static void *iommu_dma_alloc(struct device *dev, size_t size,
 
 	gfp |= __GFP_ZERO;
 
-	if (IS_ENABLED(CONFIG_DMA_REMAP) && gfpflags_allow_blocking(gfp) &&
+	if (gfpflags_allow_blocking(gfp) &&
 	    !(attrs & DMA_ATTR_FORCE_CONTIGUOUS)) {
 		return iommu_dma_alloc_remap(dev, size, handle, gfp,
 				dma_pgprot(dev, PAGE_KERNEL, attrs), attrs);
@@ -1072,7 +1072,7 @@ static int iommu_dma_mmap(struct device *dev, struct vm_area_struct *vma,
 	if (off >= nr_pages || vma_pages(vma) > nr_pages - off)
 		return -ENXIO;
 
-	if (IS_ENABLED(CONFIG_DMA_REMAP) && is_vmalloc_addr(cpu_addr)) {
+	if (is_vmalloc_addr(cpu_addr)) {
 		struct page **pages = dma_common_find_pages(cpu_addr);
 
 		if (pages)
@@ -1094,7 +1094,7 @@ static int iommu_dma_get_sgtable(struct device *dev, struct sg_table *sgt,
 	struct page *page;
 	int ret;
 
-	if (IS_ENABLED(CONFIG_DMA_REMAP) && is_vmalloc_addr(cpu_addr)) {
+	if (is_vmalloc_addr(cpu_addr)) {
 		struct page **pages = dma_common_find_pages(cpu_addr);
 
 		if (pages) {
