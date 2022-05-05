5388b7ea6dfa42788d2a8a21167b6df27a437edf
Indan Zupancic <Indan.Zupancic@mep-info.com>
2022-05-05 13:47:50 +0200

    fsl_lpuart: Don't enable interrupts too early
    Source: Kernel.org
    MR: 124537
    Type: Integration
    Disposition: Backport from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-5.10.y
    ChangeID: 2ecf0819e441c074e18c543bcdb8a6f48cc98d3f
    Description:
    
    commit 401fb66a355eb0f22096cf26864324f8e63c7d78 upstream.
    
    If an irq is pending when devm_request_irq() is called, the irq
    handler will cause a NULL pointer access because initialisation
    is not done yet.
    
    Fixes: 9d7ee0e28da59 ("tty: serial: lpuart: avoid report NULL interrupt")
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Indan Zupancic <Indan.Zupancic@mep-info.com>
    Link: https://lore.kernel.org/r/20220505114750.45423-1-Indan.Zupancic@mep-info.com
    [5.10 did not have lpuart_global_reset or anything after
    uart_add_one_port(), so add the remove call in cleanup manually]
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- drivers/tty/serial/fsl_lpuart.c
+++ drivers/tty/serial/fsl_lpuart.c
@@ -2843,6 +2843,7 @@ static int lpuart_probe(struct platform_device *pdev)
 	struct device_node *np = pdev->dev.of_node;
 	struct lpuart_port *sport;
 	struct resource *res;
+	irq_handler_t handler;
 	int ret;
 
 	sport = devm_kzalloc(&pdev->dev, sizeof(*sport), GFP_KERNEL);
@@ -2922,12 +2923,10 @@ static int lpuart_probe(struct platform_device *pdev)
 
 	if (lpuart_is_32(sport)) {
 		lpuart_reg.cons = LPUART32_CONSOLE;
-		ret = devm_request_irq(&pdev->dev, sport->port.irq, lpuart32_int, 0,
-					DRIVER_NAME, sport);
+		handler = lpuart32_int;
 	} else {
 		lpuart_reg.cons = LPUART_CONSOLE;
-		ret = devm_request_irq(&pdev->dev, sport->port.irq, lpuart_int, 0,
-					DRIVER_NAME, sport);
+		handler = lpuart_int;
 	}
 
 	if (ret)
@@ -2960,8 +2959,14 @@ static int lpuart_probe(struct platform_device *pdev)
 	if (ret)
 		goto failed_attach_port;
 
+	ret = devm_request_irq(&pdev->dev, sport->port.irq, handler, 0,
+				DRIVER_NAME, sport);
+	if (ret)
+		goto failed_irq_request;
+
 	return 0;
 
+failed_irq_request:
 failed_get_rs485:
 failed_reset:
 	uart_remove_one_port(&lpuart_reg, &sport->port);
@@ -2969,7 +2974,6 @@ failed_attach_port:
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_set_suspended(&pdev->dev);
 	pm_runtime_dont_use_autosuspend(&pdev->dev);
-failed_irq_request:
 	lpuart_disable_clks(sport);
 	return ret;
 }
