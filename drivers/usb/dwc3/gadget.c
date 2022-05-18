9a34b9e6b1d467e1303e0158ee2b656f3fc9f4d1
Albert Wang <albertccwang@google.com>
2022-05-18 14:13:15 +0800

    usb: dwc3: gadget: Move null pinter check to proper place
    Source: Kernel.org
    MR: 118429
    Type: Security Fix
    Disposition: Backport from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-5.10.y
    ChangeID: a2532c441705c406f07f55afe586af54e9cf4041
    Description:
    
    commit 3c5880745b4439ac64eccdb040e37fc1cc4c5406 upstream.
    
    When dwc3_gadget_ep_cleanup_completed_requests() called to
    dwc3_gadget_giveback() where the dwc3 lock is released, other thread is
    able to execute. In this situation, usb_ep_disable() gets the chance to
    clear endpoint descriptor pointer which leds to the null pointer
    dereference problem. So needs to move the null pointer check to a proper
    place.
    
    Example call stack:
    
    Thread#1:
    dwc3_thread_interrupt()
      spin_lock
      -> dwc3_process_event_buf()
       -> dwc3_process_event_entry()
        -> dwc3_endpoint_interrupt()
         -> dwc3_gadget_endpoint_trbs_complete()
          -> dwc3_gadget_ep_cleanup_completed_requests()
           ...
           -> dwc3_giveback()
              spin_unlock
              Thread#2 executes
    
    Thread#2:
    configfs_composite_disconnect()
      -> __composite_disconnect()
       -> ffs_func_disable()
        -> ffs_func_set_alt()
         -> ffs_func_eps_disable()
          -> usb_ep_disable()
             wait for dwc3 spin_lock
             Thread#1 released lock
             clear endpoint.desc
    
    Fixes: 26288448120b ("usb: dwc3: gadget: Fix null pointer exception")
    Cc: stable <stable@kernel.org>
    Signed-off-by: Albert Wang <albertccwang@google.com>
    Link: https://lore.kernel.org/r/20220518061315.3359198-1-albertccwang@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -3053,9 +3053,6 @@ static bool dwc3_gadget_endpoint_trbs_complete(struct dwc3_ep *dep,
 	struct dwc3		*dwc = dep->dwc;
 	bool			no_started_trb = true;
 
-	if (!dep->endpoint.desc)
-		return no_started_trb;
-
 	dwc3_gadget_ep_cleanup_completed_requests(dep, event, status);
 
 	if (dep->stream_capable && !list_empty(&dep->started_list))
@@ -3063,6 +3060,9 @@ static bool dwc3_gadget_endpoint_trbs_complete(struct dwc3_ep *dep,
 
 	if (dep->flags & DWC3_EP_END_TRANSFER_PENDING)
 		goto out;
+
+	if (!dep->endpoint.desc)
+		return no_started_trb;
 
 	if (usb_endpoint_xfer_isoc(dep->endpoint.desc) &&
 	    list_empty(&dep->started_list)) {
