f5e7573fcdf67d4fbe0e813a1df1c896dd4d5a47
Michael Grzeschik <m.grzeschik@pengutronix.de>
2022-07-20 23:35:23 +0200

    usb: dwc3: gadget: conditionally remove requests
    Source: Kernel.org
    MR: 124161
    Type: Integration
    Disposition: Backport from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-5.10.y
    ChangeID: a32635528d6552cb729968af2db09e64c4ac6f24
    Description:
    
    [ Upstream commit b44c0e7fef51ee7e8ca8c6efbf706f5613787100 ]
    
    The functions stop_active_transfers and ep_disable are both calling
    remove_requests. This functions in both cases will giveback the requests
    with status ESHUTDOWN, which also represents an physical disconnection.
    For ep_disable this is not true. This patch adds the status parameter to
    remove_requests and sets the status to ECONNRESET on ep_disable.
    
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    Link: https://lore.kernel.org/r/20220720213523.1055897-1-m.grzeschik@pengutronix.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: f90f5afd5083 ("usb: dwc3: gadget: Clear ep descriptor last")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -768,7 +768,7 @@ out:
 	return 0;
 }
 
-static void dwc3_remove_requests(struct dwc3 *dwc, struct dwc3_ep *dep)
+static void dwc3_remove_requests(struct dwc3 *dwc, struct dwc3_ep *dep, int status)
 {
 	struct dwc3_request		*req;
 
@@ -778,19 +778,19 @@ static void dwc3_remove_requests(struct dwc3 *dwc, struct dwc3_ep *dep)
 	while (!list_empty(&dep->started_list)) {
 		req = next_request(&dep->started_list);
 
-		dwc3_gadget_giveback(dep, req, -ESHUTDOWN);
+		dwc3_gadget_giveback(dep, req, status);
 	}
 
 	while (!list_empty(&dep->pending_list)) {
 		req = next_request(&dep->pending_list);
 
-		dwc3_gadget_giveback(dep, req, -ESHUTDOWN);
+		dwc3_gadget_giveback(dep, req, status);
 	}
 
 	while (!list_empty(&dep->cancelled_list)) {
 		req = next_request(&dep->cancelled_list);
 
-		dwc3_gadget_giveback(dep, req, -ESHUTDOWN);
+		dwc3_gadget_giveback(dep, req, status);
 	}
 }
 
@@ -825,7 +825,7 @@ int __dwc3_gadget_ep_disable(struct dwc3_ep *dep)
 		dep->endpoint.desc = NULL;
 	}
 
-	dwc3_remove_requests(dwc, dep);
+	dwc3_remove_requests(dwc, dep, -ECONNRESET);
 
 	dep->stream_capable = false;
 	dep->type = 0;
@@ -2117,7 +2117,7 @@ static void dwc3_stop_active_transfers(struct dwc3 *dwc)
 		if (!dep)
 			continue;
 
-		dwc3_remove_requests(dwc, dep);
+		dwc3_remove_requests(dwc, dep, -ESHUTDOWN);
 	}
 }
 
