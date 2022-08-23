1ac4bbea5d14279481a792bace1a31390572c61d
Kuniyuki Iwashima <kuniyu@amazon.com>
2022-08-23 10:46:53 -0700

    net: Fix a data-race around netdev_budget.
    Source: Kernel.org
    MR: 122563
    Type: Integration
    Disposition: Backport from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-4.14.y
    ChangeID: c9328c3d5a9eb21da020c7a9490964554963d2eb
    Description:
    
    [ Upstream commit 2e0c42374ee32e72948559d2ae2f7ba3dc6b977c ]
    
    While reading netdev_budget, it can be changed concurrently.
    Thus, we need to add READ_ONCE() to its reader.
    
    Fixes: 51b0bdedb8e7 ("[NET]: Separate two usages of netdev_max_backlog.")
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5669,7 +5669,7 @@ static __latent_entropy void net_rx_action(struct softirq_action *h)
 	struct softnet_data *sd = this_cpu_ptr(&softnet_data);
 	unsigned long time_limit = jiffies +
 		usecs_to_jiffies(netdev_budget_usecs);
-	int budget = netdev_budget;
+	int budget = READ_ONCE(netdev_budget);
 	struct sk_buff_head tofree_q;
 	struct sk_buff *skb;
 	LIST_HEAD(list);
