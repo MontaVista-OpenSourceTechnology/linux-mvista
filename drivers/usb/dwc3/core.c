0af9a70b8ec8e993b087d0a28eead13cfcc8ff6a
Piyush Mehta <piyush.mehta@amd.com>
2022-09-20 10:52:35 +0530

    usb: dwc3: core: Enable GUCTL1 bit 10 for fixing termination error after resume bug
    Source: Kernel.org
    MR: 169127
    Type: Integration
    Disposition: Backport from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-5.10.y
    ChangeID: 1ac0667658264564ceb7bb21b52dcef85860088e
    Description:
    
    [ Upstream commit 63d7f9810a38102cdb8cad214fac98682081e1a7 ]
    
    When configured in HOST mode, after issuing U3/L2 exit controller fails
    to send proper CRC checksum in CRC5 field. Because of this behavior
    Transaction Error is generated, resulting in reset and re-enumeration of
    usb device attached. Enabling chicken bit 10 of GUCTL1 will correct this
    problem.
    
    When this bit is set to '1', the UTMI/ULPI opmode will be changed to
    "normal" along with HS terminations, term, and xcvr signals after EOR.
    This option is to support certain legacy UTMI/ULPI PHYs.
    
    Added "snps,resume-hs-terminations" quirk to resolved the above issue.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@amd.com>
    Link: https://lore.kernel.org/r/20220920052235.194272-3-piyush.mehta@amd.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: 9149c9b0c7e0 ("usb: dwc3: core: update LC timer as per USB Spec V3.2")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1121,6 +1121,7 @@ int dwc3_core_init(struct dwc3 *dwc)
 		dwc3_writel(dwc->regs, DWC3_GUCTL2, reg);
 	}
 
+<<<<<<< HEAD
 	/* When configured in HOST mode, after issuing U3/L2 exit controller
 	 * fails to send proper CRC checksum in CRC5 feild. Because of this
 	 * behaviour Transaction Error is generated, resulting in reset and
@@ -1144,6 +1145,20 @@ int dwc3_core_init(struct dwc3 *dwc)
 	if (dwc->enable_guctl1_ipd_quirk) {
 		reg = dwc3_readl(dwc->regs, DWC3_GUCTL1);
 		reg |= DWC3_GUCTL1_IPD_QUIRK;
+=======
+	/*
+	 * When configured in HOST mode, after issuing U3/L2 exit controller
+	 * fails to send proper CRC checksum in CRC5 feild. Because of this
+	 * behaviour Transaction Error is generated, resulting in reset and
+	 * re-enumeration of usb device attached. All the termsel, xcvrsel,
+	 * opmode becomes 0 during end of resume. Enabling bit 10 of GUCTL1
+	 * will correct this problem. This option is to support certain
+	 * legacy ULPI PHYs.
+	 */
+	if (dwc->resume_hs_terminations) {
+		reg = dwc3_readl(dwc->regs, DWC3_GUCTL1);
+		reg |= DWC3_GUCTL1_RESUME_OPMODE_HS_HOST;
+>>>>>>> b6160f2f23d3 (usb: dwc3: core: Enable GUCTL1 bit 10 for fixing termination error after resume bug)
 		dwc3_writel(dwc->regs, DWC3_GUCTL1, reg);
 	}
 
@@ -1476,6 +1491,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 				"snps,dis-del-phy-power-chg-quirk");
 	dwc->dis_tx_ipgap_linecheck_quirk = device_property_read_bool(dev,
 				"snps,dis-tx-ipgap-linecheck-quirk");
+	dwc->resume_hs_terminations = device_property_read_bool(dev,
+				"snps,resume-hs-terminations");
 	dwc->parkmode_disable_ss_quirk = device_property_read_bool(dev,
 				"snps,parkmode-disable-ss-quirk");
 
