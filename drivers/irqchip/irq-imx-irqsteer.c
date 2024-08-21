a135901283e5b1cc269287493eff1968c101ee45
Armin Kuster <akuster@mvista.com>
2024-08-21 14:59:19 -0400

    Fix miss merge
    Source: MontaVista Software, LLC
    MR: 160524
    Type: Integration
    Disposition: Local
    ChangeID: 84bd8e98514f566c72d94b1c2501cf660caf8de1
    Description:
    
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- drivers/irqchip/irq-imx-irqsteer.c
+++ drivers/irqchip/irq-imx-irqsteer.c
@@ -406,8 +406,8 @@ static int imx_irqsteer_runtime_resume(struct device *dev)
 static const struct dev_pm_ops imx_irqsteer_pm_ops = {
 	SET_NOIRQ_SYSTEM_SLEEP_PM_OPS(pm_runtime_force_suspend,
 				      pm_runtime_force_resume)
-	SET_RUNTIME_PM_OPS(imx_irqsteer_suspend,
-			   imx_irqsteer_resume, NULL)
+	SET_RUNTIME_PM_OPS(imx_irqsteer_runtime_suspend,
+			   imx_irqsteer_runtime_resume, NULL)
 };
 
 static const struct of_device_id imx_irqsteer_dt_ids[] = {
