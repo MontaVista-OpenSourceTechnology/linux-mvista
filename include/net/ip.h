ac76572cac02d12e86c5eb5a0b96d0b0871989d4
Eric Dumazet <edumazet@google.com>
2025-02-05 15:51:11 +0000

    ipv4: use RCU protection in ip_dst_mtu_maybe_forward()
    Source: https://git.kernel.org
    MR: 439575
    Type: Security Fix
    Disposition: Backport from v6.14-rc3
    ChangeID: 071d8012869b6af352acca346ade13e7be90a49f
    Description:
    
    CVE-2025-68188 kernel: tcp: use dst_dev_rcu() in
    tcp_fastopen_active_disable_ofo_check()
    
    Dependency Patch #30.
    
    ip_dst_mtu_maybe_forward() must use RCU protection to make
    sure the net structure it reads does not disappear.
    
    Fixes: f87c10a8aa1e8 ("ipv4: introduce ip_dst_mtu_maybe_forward and protect forwarding path against pmtu spoofing")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Link: https://patch.msgid.link/20250205155120.1676781-4-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/net/ip.h
+++ include/net/ip.h
@@ -440,9 +440,12 @@ static inline bool ip_sk_ignore_df(const struct sock *sk)
 static inline unsigned int ip_dst_mtu_maybe_forward(const struct dst_entry *dst,
 						    bool forwarding)
 {
-	struct net *net = dev_net(dst->dev);
-	unsigned int mtu;
+	unsigned int mtu, res;
+	struct net *net;
 
+	rcu_read_lock();
+
+	net = dev_net_rcu(dst->dev);
 	if (READ_ONCE(net->ipv4.sysctl_ip_fwd_use_pmtu) ||
 	    ip_mtu_locked(dst) ||
 	    !forwarding)
@@ -453,7 +456,11 @@ static inline unsigned int ip_dst_mtu_maybe_forward(const struct dst_entry *dst,
 	if (!mtu)
 		mtu = min(READ_ONCE(dst->dev->mtu), IP_MAX_MTU);
 
-	return mtu - lwtunnel_headroom(dst->lwtstate, mtu);
+	res = mtu - lwtunnel_headroom(dst->lwtstate, mtu);
+
+	rcu_read_unlock();
+
+	return res;
 }
 
 static inline unsigned int ip_skb_dst_mtu(struct sock *sk,
