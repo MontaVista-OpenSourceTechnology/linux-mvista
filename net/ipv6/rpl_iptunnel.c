8cc27075a8089bdb11898bf06dcdc80c4553bc81
Eric Dumazet <edumazet@google.com>
2025-06-30 12:19:32 +0000

    ipv6: adopt dst_dev() helper
    Source: https://git.kernel.org
    MR: 439575
    Type: Security Fix
    Disposition: Backport from v6.17-rc1
    ChangeID: 1caf27297215a5241f9bfc9c07336349d9034ee3
    Description:
    
    CVE-2025-68188 kernel: tcp: use dst_dev_rcu() in
    tcp_fastopen_active_disable_ofo_check()
    
    Dependency Patch #38.
    
    Use the new helper as a step to deal with potential dst->dev races.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Link: https://patch.msgid.link/20250630121934.3399505-9-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- net/ipv6/rpl_iptunnel.c
+++ net/ipv6/rpl_iptunnel.c
@@ -239,7 +239,7 @@ static int rpl_output(struct net *net, struct sock *sk, struct sk_buff *skb)
 		dst_cache_set_ip6(&rlwt->cache, dst, &fl6.saddr);
 		local_bh_enable();
 
-		err = skb_cow_head(skb, LL_RESERVED_SPACE(dst->dev));
+		err = skb_cow_head(skb, LL_RESERVED_SPACE(dst_dev(dst)));
 		if (unlikely(err))
 			goto drop;
 	}
@@ -296,7 +296,7 @@ static int rpl_input(struct sk_buff *skb)
 			local_bh_enable();
 		}
 
-		err = skb_cow_head(skb, LL_RESERVED_SPACE(dst->dev));
+		err = skb_cow_head(skb, LL_RESERVED_SPACE(dst_dev(dst)));
 		if (unlikely(err))
 			goto drop;
 	} else {
