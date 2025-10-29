0ea700972056616e3679391f07526cf040a07e5b
Kuniyuki Iwashima <kuniyu@google.com>
2025-10-29 17:32:59 +0000

    mpls: Pass net to mpls_dev_get().
    Source: https://git.kernel.org
    MR: 438988
    Type: Security Fix
    Disposition: Backport from v6.19-rc1
    ChangeID: 1fb462de9329731c17267c7ccf19619f22790a0a
    Description:
    
    CVE-2026-43042 kernel: mpls: add seqcount to protect the platform_label{,s}
    pair
    
    Dependency Patch #17.
    
    We will replace RTNL with a per-netns mutex to protect dev->mpls_ptr.
    
    Then, we will use rcu_dereference_protected() with the lockdep_is_held()
    annotation, which requires net to access the per-netns mutex.
    
    However, dev_net(dev) is not safe without RTNL.
    
    Let's pass net to mpls_dev_get().
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reviewed-by: Guillaume Nault <gnault@redhat.com>
    Link: https://patch.msgid.link/20251029173344.2934622-8-kuniyu@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Javier Campos <jcampos@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- net/mpls/af_mpls.c
+++ net/mpls/af_mpls.c
@@ -689,7 +689,7 @@ static int mpls_nh_assign_dev(struct net *net, struct mpls_route *rt,
 
 	/* Ensure this is a supported device */
 	err = -EINVAL;
-	if (!mpls_dev_get(dev))
+	if (!mpls_dev_get(net, dev))
 		goto errout;
 
 	if ((nh->nh_via_table == NEIGH_LINK_TABLE) &&
@@ -1272,7 +1272,7 @@ static int mpls_netconf_get_devconf(struct sk_buff *in_skb,
 	if (!dev)
 		goto errout;
 
-	mdev = mpls_dev_get(dev);
+	mdev = mpls_dev_get(net, dev);
 	if (!mdev)
 		goto errout;
 
@@ -1597,6 +1597,7 @@ static int mpls_dev_notify(struct notifier_block *this, unsigned long event,
 			   void *ptr)
 {
 	struct net_device *dev = netdev_notifier_info_to_dev(ptr);
+	struct net *net = dev_net(dev);
 	struct mpls_dev *mdev;
 	unsigned int flags;
 	int err;
@@ -1611,7 +1612,7 @@ static int mpls_dev_notify(struct notifier_block *this, unsigned long event,
 		goto out;
 	}
 
-	mdev = mpls_dev_get(dev);
+	mdev = mpls_dev_get(net, dev);
 	if (!mdev)
 		goto out;
 
@@ -1644,7 +1645,7 @@ static int mpls_dev_notify(struct notifier_block *this, unsigned long event,
 		if (err)
 			goto err;
 
-		mdev = mpls_dev_get(dev);
+		mdev = mpls_dev_get(net, dev);
 		if (mdev) {
 			mpls_dev_sysctl_unregister(dev, mdev);
 			RCU_INIT_POINTER(dev->mpls_ptr, NULL);
@@ -1652,7 +1653,7 @@ static int mpls_dev_notify(struct notifier_block *this, unsigned long event,
 		}
 		break;
 	case NETDEV_CHANGENAME:
-		mdev = mpls_dev_get(dev);
+		mdev = mpls_dev_get(net, dev);
 		if (mdev) {
 			mpls_dev_sysctl_unregister(dev, mdev);
 			err = mpls_dev_sysctl_register(dev, mdev);
