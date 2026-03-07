003a836f326b26838995b45129cdadcfe260eb97
Calvin Owens <calvin@wbinvd.org>
2026-03-06 19:19:25 -0800

    tracing: Fix trace_buf_size= cmdline parameter with sizes >= 2G
    Source: Kernel.org
    MR: 415642
    Type: Integration
    Disposition: Backport from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-5.10.y
    ChangeID: 8c1130c196562aa8dc3029ca4b9f86025a506c34
    Description:
    
    commit d008ba8be8984760e36d7dcd4adbd5a41a645708 upstream.
    
    Some of the sizing logic through tracer_alloc_buffers() uses int
    internally, causing unexpected behavior if the user passes a value that
    does not fit in an int (on my x86 machine, the result is uselessly tiny
    buffers).
    
    Fix by plumbing the parameter's real type (unsigned long) through to the
    ring buffer allocation functions, which already use unsigned long.
    
    It has always been possible to create larger ring buffers via the sysfs
    interface: this only affects the cmdline parameter.
    
    Cc: stable@vger.kernel.org
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Link: https://patch.msgid.link/bff42a4288aada08bdf74da3f5b67a2c28b761f8.1772852067.git.calvin@wbinvd.org
    Fixes: 73c5162aa362 ("tracing: keep ring buffer to minimum size till used")
    Signed-off-by: Calvin Owens <calvin@wbinvd.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -8776,7 +8776,7 @@ static void
 init_tracer_tracefs(struct trace_array *tr, struct dentry *d_tracer);
 
 static int
-allocate_trace_buffer(struct trace_array *tr, struct array_buffer *buf, int size)
+allocate_trace_buffer(struct trace_array *tr, struct array_buffer *buf, unsigned long size)
 {
 	enum ring_buffer_flags rb_flags;
 
@@ -8802,7 +8802,7 @@ allocate_trace_buffer(struct trace_array *tr, struct array_buffer *buf, int size
 	return 0;
 }
 
-static int allocate_trace_buffers(struct trace_array *tr, int size)
+static int allocate_trace_buffers(struct trace_array *tr, unsigned long size)
 {
 	int ret;
 
@@ -9731,7 +9731,7 @@ out:
 
 __init static int tracer_alloc_buffers(void)
 {
-	int ring_buf_size;
+	unsigned long ring_buf_size;
 	int ret = -ENOMEM;
 
 
