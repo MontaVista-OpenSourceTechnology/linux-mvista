4b49d0836dc31df7f59c1f150d1888789108bd2c
Johan Hovold <johan@kernel.org>
2026-04-10 10:17:55 +0200

    spi: zynqmp-gqspi: fix controller deregistration
    Source: kernel.org
    MR: 737923
    Type: Integration
    Disposition: Merged from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-5.10.y
    ChangeID: aeb00d7e89ca21c5ca08fae3e21a0338e5103445
    Description:
    
    commit 6895fc4faafc9082e15e4e624b23dd5f0c98feb5 upstream.
    
    Make sure to deregister the controller before disabling underlying
    resources like clocks during driver unbind.
    
    Fixes: dfe11a11d523 ("spi: Add support for Zynq Ultrascale+ MPSoC GQSPI controller")
    Cc: stable@vger.kernel.org	# 4.2: 64640f6c972e
    Cc: stable@vger.kernel.org	# 4.2
    Cc: Ranjit Waghmode <ranjit.waghmode@xilinx.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Link: https://patch.msgid.link/20260410081757.503099-26-johan@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- drivers/spi/spi-zynqmp-gqspi.c
+++ drivers/spi/spi-zynqmp-gqspi.c
@@ -1392,7 +1392,7 @@ static int zynqmp_qspi_probe(struct platform_device *pdev)
 	ctlr->dev.of_node = np;
 	ctlr->auto_runtime_pm = true;
 
-	ret = devm_spi_register_controller(&pdev->dev, ctlr);
+	ret = spi_register_controller(ctlr);
 	if (ret) {
 		dev_err(&pdev->dev, "spi_register_controller failed\n");
 		goto clk_dis_all;
@@ -1429,6 +1429,8 @@ remove_master:
 static int zynqmp_qspi_remove(struct platform_device *pdev)
 {
 	struct zynqmp_qspi *xqspi = platform_get_drvdata(pdev);
+
+	spi_unregister_controller(xqspi->ctlr);
 
 	zynqmp_gqspi_write(xqspi, GQSPI_EN_OFST, 0x0);
 	clk_disable_unprepare(xqspi->refclk);
