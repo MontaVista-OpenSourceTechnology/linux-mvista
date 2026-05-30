b14c1059d48df168c699fe3491769cda23ae48ac
Al Viro <viro@zeniv.linux.org.uk>
2026-05-30 10:19:23 -0400

    use less confusing names for iov_iter direction initializers
    Source: kernel.org
    MR: 737923
    Type: Integration
    Disposition: Merged from git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable linux-5.10.y
    ChangeID: 39a5787c9eb3e8c47d121843973ca7225c0f2a35
    Description:
    
    [ Upstream commit de4eda9de2d957ef2d6a8365a01e26a435e958cb ]
    
    READ/WRITE proved to be actively confusing - the meanings are
    "data destination, as used with read(2)" and "data source, as
    used with write(2)", but people keep interpreting those as
    "we read data from it" and "we write data to it", i.e. exactly
    the wrong way.
    
    Call them ITER_DEST and ITER_SOURCE - at least that is harder
    to misinterpret...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Stable-dep-of: a4f0b001782b ("vsock/virtio: reset connection on receiving queue overflow")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -3179,7 +3179,7 @@ static ssize_t __cifs_writev(
 		ctx->iter = *from;
 		ctx->len = len;
 	} else {
-		rc = setup_aio_ctx_iter(ctx, from, WRITE);
+		rc = setup_aio_ctx_iter(ctx, from, ITER_SOURCE);
 		if (rc) {
 			kref_put(&ctx->refcount, cifs_aio_ctx_release);
 			return rc;
@@ -3918,7 +3918,7 @@ static ssize_t __cifs_readv(
 		ctx->iter = *to;
 		ctx->len = len;
 	} else {
-		rc = setup_aio_ctx_iter(ctx, to, READ);
+		rc = setup_aio_ctx_iter(ctx, to, ITER_DEST);
 		if (rc) {
 			kref_put(&ctx->refcount, cifs_aio_ctx_release);
 			return rc;
